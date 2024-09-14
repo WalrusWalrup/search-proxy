22025c3a783a758503931869cc02ed0346a95272
Walrus <mrwalrus2023@gmail.com>
2024-09-14 21:48:03 +1000

    Updates proxy and index files to add search functionality with custom header

--- proxy.py
+++ proxy.py
@@ -1,37 +1,15 @@
-from flask import Flask, request, redirect, render_template_string
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-HTML_TEMPLATE = '''
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Proxy Search</title>
-</head>
-<body>
-    <h1>Proxy Search</h1>
-    <form action="/redirect" method="get">
-        <input type="text" name="url" placeholder="Enter URL here" size="50" autofocus>
-        <button type="submit">Go</button>
-    </form>
-</body>
-</html>
-'''
-
 @app.route('/')
 def index():
-    return render_template_string(HTML_TEMPLATE)
+    return render_template('index.html', query=None)
 
-@app.route('/redirect')
-def redirect_to_url():
-    url = request.args.get('url')
-    if not url:
-        return 'Missing URL parameter', 400
-    if not url.startswith(('http://', 'https://')):
-        url = 'http://' + url
-    return redirect(url)
+@app.route('/search')
+def search():
+    query = request.args.get('q')
+    return render_template('index.html', query=query)
 
 if __name__ == '__main__':
     app.run(port=8000)
